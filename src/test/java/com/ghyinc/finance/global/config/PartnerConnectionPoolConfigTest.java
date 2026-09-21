6955c3d7f80e26c1edd91b9d89903461e3a55f20
gyohee91 <gyohee91@gmail.com>
2026-09-21 16:51:04 +0900

    한도결과 API - 한도결과 로직과 COUNT Update 트랜잭션 분리 (테스트코드 오류)

--- src/test/java/com/ghyinc/finance/global/config/PartnerConnectionPoolConfigTest.java
+++ src/test/java/com/ghyinc/finance/global/config/PartnerConnectionPoolConfigTest.java
@@ -68,6 +68,6 @@ class PartnerConnectionPoolConfigTest {
         PoolingHttpClientConnectionManager connectionManager = partnerConnectionPoolConfig.partnerConnectionManager();
         HttpRoute httpRoute = new HttpRoute(new HttpHost("http", "kakaobank-mock", 8091));
 
-        assertThat(connectionManager.getMaxPerRoute(httpRoute)).isEqualTo(15);
+        assertThat(connectionManager.getMaxPerRoute(httpRoute)).isEqualTo(20);
     }
 }
